f89aa87ced3480845ba998643dfe4a2b51879e81
Maxim Vasekha <95barca7@gmail.com>
2024-04-28 14:19:40 +0300

    new

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -13,7 +13,7 @@ public class AppConfig {
     }
 
     @Bean(name = "cat")
-    @Scope(value = "prototype")
+    @Scope("prototype")
     public Cat getCat() {
         Cat cat = new Cat();
         cat.setMassage("Hello Cat!");
